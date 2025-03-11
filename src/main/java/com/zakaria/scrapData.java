35a2953a1c972737bda1cad75b570e174e0a311b
ZakariaOuakrim <zakarialukyguy@gmail.com>
2025-03-11 22:18:48 +0000

    kafka and zookeeper Docker-compose

--- src/main/java/com/zakaria/scrapData.java
+++ src/main/java/com/zakaria/scrapData.java
@@ -1,11 +1,15 @@
 package com.zakaria;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class scrapData {
     public static void main(String args[]) {
@@ -21,20 +25,38 @@ public class scrapData {
             }
             // Extract table headers
             Elements headers = table.select("thead th");
+            List<String> headerList=new ArrayList<>();
             System.out.println("Headers:");
             for (Element header : headers) {
+                String tableHeader=header.text();
+                headerList.add(tableHeader);
                 System.out.print(header.text() + "\t");
             }
+
+            JSONArray jsonArray = new JSONArray();
 
             // Extract table rows
             Elements rows = table.select("tbody tr");
             for (Element row : rows) {
                 Elements columns = row.select("td");
+                JSONObject teamObject = new JSONObject();
+
+                for(int i=0;i<columns.size() && i < headerList.size();i++){
+                    teamObject.put(headerList.get(i),columns.get(i).text());
+                }
+
+                jsonArray.put(teamObject);
+
                 for (Element column : columns) {
                     System.out.print(column.text() + "\t");
                 }
                 System.out.println();
             }
+
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("teams", jsonArray);
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
